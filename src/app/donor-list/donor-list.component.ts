47aaeb3b5d183f1d126e9a080eecd755aab03706
sammy <sammy@MacBook-Pro-de-sammy.local>
2019-09-20 10:33:13 +0200

    donor-list map

--- src/app/donor-list/donor-list.component.ts
+++ src/app/donor-list/donor-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
-@ViewChild('map', {static: false}) mapElement: ElementRef;
+
+
 
 declare var google: any;
 let map: any;
@@ -13,49 +14,50 @@ const options = {
 let infowindow: any;
 const iconBase = 'http://maps.google.com/mapfiles/ms/icons/';
 
-
-initMap() {
-  navigator.geolocation.getCurrentPosition((location) => {
-    map = new google.maps.Map(this.mapElement.nativeElement, {
-      center: { lat: location.coords.latitude, lng: location.coords.longitude },
-      zoom: 15
-    });
-
-    infowindow = new google.maps.InfoWindow();
-
-
-    marker = new google.maps.Marker({
-      position: { lat: location.coords.latitude, lng: location.coords.longitude },
-      map,
-      title: 'Click to zoom',
-      icon: iconBase + 'blue-dot.png'
-    });
-
-    map.addListener('center_changed', () => {
-      window.setTimeout(() => {
-        map.panTo(marker.getPosition());
-      }, 3000);
-    });
-
-    marker.addListener('click', (event: any) => {
-      infowindow.setPosition(event.latLng);
-      infowindow.setContent('<h2>Yes, I wanna be a donor!</h2>' +
-      '<h3><a href="/add-donor/' + marker.getPosition().lat() + '/' + marker.getPosition().lng()  + '">Register Here</a></h3>');
-      infowindow.open(map, marker);
-    });
-  }, (error) => {
-    console.log(error);
-  }, options);
-}
-
 @Component({
   selector: 'app-donor-list',
   templateUrl: './donor-list.component.html',
   styleUrls: ['./donor-list.component.scss']
 })
 export class DonorListComponent implements OnInit {
+  @ViewChild('map', {static: false}) mapElement: ElementRef;
 
-  constructor() { }
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: { lat: location.coords.latitude, lng: location.coords.longitude },
+        zoom: 15
+      });
+  
+      infowindow = new google.maps.InfoWindow();
+  
+  
+      marker = new google.maps.Marker({
+        position: { lat: location.coords.latitude, lng: location.coords.longitude },
+        map,
+        title: 'Click to zoom',
+        icon: iconBase + 'blue-dot.png'
+      });
+  
+      map.addListener('center_changed', () => {
+        window.setTimeout(() => {
+          map.panTo(marker.getPosition());
+        }, 3000);
+      });
+  
+      marker.addListener('click', (event: any) => {
+        infowindow.setPosition(event.latLng);
+        infowindow.setContent('<h2>Yes, I wanna be a donor!</h2>' +
+        '<h3><a href="/add-donor/' + marker.getPosition().lat() + '/' + marker.getPosition().lng()  + '">Register Here</a></h3>');
+        infowindow.open(map, marker);
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+  }
+  constructor() {
+    this.initMap();
+  }
 
   ngOnInit() {
   }
